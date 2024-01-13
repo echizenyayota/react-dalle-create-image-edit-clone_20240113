3e337b4dcad425dee6ff6657ed3f9750ccf9583d
echizenyayota <virtualmisso30@gmail.com>
2024-01-13 22:05:35 +0900

    Launch a web server

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const PORT = 8000;
 const express = require("express");
+
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Testing whether the API works",
+  }); 
+});
+
+app.listen(PORT, () => console.log("Your server is running on PORT " + PORT));  
