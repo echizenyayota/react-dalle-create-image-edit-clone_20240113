a25ce2e058486c8cb90d0310758bb7f0ea2eea0a
echizenyayota <virtualmisso30@gmail.com>
2024-01-13 21:38:30 +0900

    Place the elements to upload the image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { upload } from "@testing-library/user-event/dist/upload";
+
 const App = () => {
+
+  const uploadImage = () => {
+
+  }
+
   return (
     <div className="app">
-
+      <div className="upload-image">
+        <span>
+          <label htmlFor="files">Upload an Image </label>
+          <input onChange={uploadImage} id="files" accept="image/*" type="file" hidden />
+          to edit
+        </span>
+      </div>
     </div>
   );
 }
