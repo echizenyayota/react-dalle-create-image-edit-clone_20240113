e714a4d8e35ab2aad418a04842fd2fed35fdb12e
echizenyayota <virtualmisso30@gmail.com>
2024-01-15 19:06:48 +0900

    Create the uploadImage function to send the image from the frontend to the backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 const App = () => {
 
-  const uploadImage = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const uploadImage = async (e) => {
+    const formData = new FormData();
+    formData.append('file', e.target.files[0]);
+    setSelectedImage(e.target.files[0]);
+
+    try {
+      const options = {
+        method: "POST",
+        body: formData,
+      }
+      const response = await fetch('http://localhost:8000/upload', options);
+      const data = await response.json();
+      console.log(data);
+    } catch(error) {
+      console.error(error);
+    }
 
   }
 
