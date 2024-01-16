32ae766aa7a14288de778cf8f3a99b2cdbde6172
echizenyayota <virtualmisso30@gmail.com>
2024-01-16 21:22:57 +0900

    Save the image in the public fold to send to the backend

--- server.js
+++ server.js
@@ -15,12 +15,36 @@ const openai = new OpenAI({
 });
 
 app.use(express.json());
-require("dotenv").config();
+
+const fs = require('fs');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage }).single('file');
 
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Testing whether the API works",
   }); 
+});
+
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+    console.log(req.file);
+  });
 });
 
 app.listen(PORT, () => console.log("Your server is running on PORT " + PORT));  
