10f27911ee2e1638b0c6826e9dce4abfdef2347c
echizenyayota <virtualmisso30@gmail.com>
2024-01-16 22:32:25 +0900

    Set the image size as 256 * 256 to generateEditImage

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 const Modal = ({ setModalOpen, setSelectedImage, selectedImage }) => {
 
   const [error, setError] = useState(null);
+  const ref = useReducer(null);
 
   const closeModal = () => {
     setModalOpen(null);
     setSelectedImage(null);
   }
 
+  const checkSize = () => {
+    if (ref.current.width === 256 && ref.current.height === 256) {
+      // generatedEditImage();
+    } else {
+      setError('Error: Choose 256 * 256')
+    }
+  }
+
   return (
     <div className="modal">
       <div onClick={closeModal}>X</div>
       <div className="image-container">
-        {selectedImage && <img src={URL.createObjectURL(selectedImage)} alt="uploaded Image"/>}
+        {selectedImage && <img ref={ref} src={URL.createObjectURL(selectedImage)} alt="uploaded Image"/>}
       </div>
-      <input />
-      <button>Send</button>
+      <p>{error || "* An Image must be 256 * 256!"}</p>
+      {!error && <input />}
+      {!error && <button onClick={checkSize}>Send</button>}
+      {error && <button onClick={closeModal}>Close this and try again</button>}
     </div>
   );
 }
